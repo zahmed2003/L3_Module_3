062202a4c388adf8b6cf660e000411850ab87a9c
zahmed2003 <league@WTS7.attlocal.net>
2018-04-28 14:22:21 -0700

    28 April 2018

--- src/interfaces/MixedCapsString.java
+++ src/interfaces/MixedCapsString.java
@@ -10,7 +10,13 @@ public class MixedCapsString extends SpecialString{
 	@Override
 	public String funkifyText(String text) {
 		
-	text = "tHiS Is aLsO A TeSt";
+	for(int i = 0; i < text.length(); i++)
+	{
+		if(i%2 != 0)
+		{
+			text = text.substring(0, i) + Character.toUpperCase(text.charAt(i)) + text.substring(i + 1);
+		}
+	}
 		
 		return text;
 	}
